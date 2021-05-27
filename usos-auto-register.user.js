508a729dea9b9db3725bcda82be73f63d5a064b4
Kasper Seweryn <github@wvffle.net>
2021-05-27 02:10:56 +0000

    Add notifications and run on countdown complete

--- usos-auto-register.user.js
+++ usos-auto-register.user.js
@@ -1,29 +1,59 @@
 // ==UserScript==
-// @name     USOS Auto Register
-// @version  1.0.0
-// @namespace https://wvffle.net/
-// @include  /https?:\/\/usosweb\.pb\.edu\.pl\/kontroler\.php.*/
-// @grant    none
+// @name        USOS Auto Register
+// @version     1.1.1
+// @author      Kasper Seweryn
+// @namespace   https://wvffle.net/
+// @supportURL  https://github.com/pb-students/userscripts
+// @downloadURL https://raw.githubusercontent.com/pb-students/userscripts/main/usos-auto-register.user.js
+// @updateURL   https://raw.githubusercontent.com/pb-students/userscripts/main/usos-auto-register.user.js
+// @include     /https?:\/\/usosweb\.pb\.edu\.pl\/kontroler\.php.*/
+// @require     https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js
+// @require     https://cdn.jsdelivr.net/npm/toastr@2.1.4/toastr.min.js
+// @grant       GM_addElement
+// ==/UserScript==
 
-const form = document.querySelector('.greenforms.ajaxForm')
-const register = () => {
+GM_addElement('link', {
+  rel: 'stylesheet',
+  href: 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css'
+})
+
+;(() => {
+  const form = document.querySelector('.greenforms.ajaxForm')
+
+  if (!form) {
+    return
+  }
+
+  const register = () => {
     fetch(form.action, {
-        method : "POST",
-        body: new FormData(form),
+      method : "POST",
+      body: new FormData(form),
     }).then(async response => {
-		const json = await response.json()
-		if (json.type === '!') throw new Error(json.pl)
-		alert("Pomyslnie zarejestrowano!")
-    }).catch(err => {
-		console.error(err)
-		setTimeout(register, 35000)
-	})
-}
+      const json = await response.json()
+      if (json.type === '!') {
+        throw new Error(json.pl)
+      }
 
-for (const x of form.querySelectorAll('.submit')) {
-  const k = document.createElement('button')
-  k.className = 'submit semitransparent'
-  k.innerText = 'REJESTRUJ AUTOMATYCZNIE'
-  k.addEventListener('click', register)
-  x.parentElement.appendChild(k)
-}
+      toastr.success('Udalo sie zapisac.', 'Yay!', { timeOut: 0 })
+    }).catch(err => {
+      console.error(err)
+      toastr.error(err.message, 'Nay :<', { timeOut: 3500 })
+      setTimeout(register, 35000)
+    })
+  }
+
+  // NOTE: Yup, they did it. With jQuery.
+  jQuery(document).ready(() => {
+    jQuery('.countdown').on('countdowncomplete', register)
+  })
+
+  // Add auto register button
+  for (const x of form.querySelectorAll('.submit')) {
+    const k = document.createElement('button')
+    k.className = 'submit semitransparent'
+    k.innerText = 'REJESTRUJ AUTOMATYCZNIE'
+    k.addEventListener('click', register)
+    x.parentElement.appendChild(k)
+  }
+})()
+
